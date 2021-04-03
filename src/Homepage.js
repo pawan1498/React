7ad2e2924e5a8bef25367f741993558d91227146
gurdayal <guru.gps.20@gmail.com>
2021-04-03 22:59:42 +0530

    carousel update

--- src/Homepage.js
+++ src/Homepage.js
@@ -1,9 +1,43 @@
 import './Homepage.css'
+import { Carousel } from 'react-bootstrap';
+import aa from './aa.webp'
+import bb from './bb.webp'
+import cc from './cc.jpg'
+import "../node_modules/bootstrap/dist/css/bootstrap.min.css";
+
 function Homepage(){
     return(
-        <>
-        {/* crousel start*/}
+        <> 
+        {/* crousel start*/} 
+        <div class="crsl">
 
+<Carousel fade >
+  <Carousel.Item>
+    <img
+      className="d-block w-100"
+      src={aa}
+      alt="First slide"
+      />
+    
+  </Carousel.Item>
+  <Carousel.Item>
+    <img
+      className="d-block w-100"
+      src={bb}
+      alt="Second slide"
+    />
+
+    
+  </Carousel.Item>
+  <Carousel.Item>
+    <img
+      className="d-block w-100"
+      src={cc}
+      alt="Third slide"
+      />
+  </Carousel.Item>
+</Carousel>
+</div>
         {/* crousel end */}
    <div class="container-fluid  aa ">
   <div class="row ">
