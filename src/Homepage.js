39694102936c0299ea18bdd54fc30cdb1671d2cd
pawan1498 <pawan.sh.7798@gmail.com>
2021-04-03 15:01:31 +0530

    add element (horizontal scroller) at homepage

--- src/Homepage.js
+++ src/Homepage.js
@@ -1,23 +1,32 @@
+import './Homepage.css'
 function Homepage(){
     return(
         <>
-        <div class="container-fluid ">
+        {/* crousel start*/}
+
+        {/* crousel end */}
+   <div class="container-fluid  aa ">
   <div class="row ">
-    <div class="col-lg-3 col-md-4 col-sm-6 col-xs-12 border text-center">
-      One of three columns
+    <div class="col-lg-2 col-sm-5  border text-center mx-3 my-1">
+      1 One of three columns
     </div>
-    <div class="col-lg-3 col-md-4 col-sm-6 col-xs-12 border text-center">
-      One of three columns
+    <div class="col-lg-2 col-sm-5  border text-center mx-3 my-1">
+      2 One of three columns
     </div>
-    <div class="col-lg-3 col-md-4 col-sm-6 col-xs-12 border text-center">
-      One of three columns
+    <div class="col-lg-2 col-sm-5  border text-center mx-3 my-1">
+      3 One of three columns
     </div> 
-    <div class="col-lg-3 col-md-4 col-sm-6 col-xs-12 border text-center">
-      One of three columns
+    <div class="col-lg-2 col-sm-5  border text-center mx-3 my-1">
+      4 One of three columns
+    </div>
+    <div class="col-lg-2 col-sm-5  border text-center mx-3 my-1">
+      5 One of three columns
+    </div>
+    <div class="col-lg-2 col-sm-5  border text-center mx-3 my-1">
+      6  One of three columns
     </div>
   </div>
-</div>
-
+ </div>
 
 {/* <div className="container border"> */}
         {/* Stack the columns on mobile by making one full-width and the other half-width */}
@@ -37,6 +46,44 @@ function Homepage(){
           {/* <div className="col-6 border">.col-6</div> */}
         {/* </div> */}
       {/* </div> */}
+
+      {/* scroll menu */}
+      <div className="container-fluid smenu">
+        <h1 className="mt-5">Deals Of The Day</h1>
+        <p className="subtitle">GRAb the best deal on home appliance ,fashion ,electronics </p>
+        <div className="scrolling-wrapper row flex-row flex-nowrap mt-4 pb-4 pt-2">
+          <div className="col">
+            <div className="card card-block card-1" />
+          </div>
+          <div className="col">
+            <div className="card card-block card-2" />
+          </div>
+          <div className="col">
+            <div className="card card-block card-3" />
+          </div>
+          <div className="col">
+            <div className="card card-block card-4" />
+          </div>
+          <div className="col">
+            <div className="card card-block card-5" />
+          </div>
+          <div className="col">
+            <div className="card card-block card-6" />
+          </div>
+          <div className="col">
+            <div className="card card-block card-7" />
+          </div>
+          <div className="col">
+            <div className="card card-block card-8" />
+          </div>
+          <div className="col">
+            <div className="card card-block card-9" />
+          </div>
+          <div className="col">
+            <div className="card card-block card-10" />
+          </div>
+        </div>
+      </div>
         </>
     );
 }
